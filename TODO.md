b47617d7a45b9d8a4526933993a56c238eefba92
Samuel Levy <samuellevy.dev@gmail.com>
2022-11-18 22:05:27 -0300

    feature: add styled components

---
+++ TODO.md
@@ -0,0 +1,6 @@
+1 - Criar tela de buscar aula
+1.1 - Uma tela simples que vamos colocar um código para buscar aula
+1.2 - Ao clicar em buscar aula, deverá redirecionar para componente de 
+videocall já com o código preenchido internamente
+2 - Criar tela de videocall
+2.1 - precisa ter N componentes de vídeo para cada client conectado
--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Home } from './pages/home';
+import Search from './pages/search';
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/call" element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
